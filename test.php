963e37c89a680187f0353ada3e34b524192bc3a2
Marc Bennewitz <marc-bennewitz@arcor.de>
2013-11-29 17:51:55 +0100

    better test script

--- test.php
+++ test.php
@@ -3,66 +3,65 @@
 error_reporting(-1);
 ini_set('display_errors', 1);
 
+$iterations = 10000;
 $t0 = 0;
 
-function cmpEqual($op1, $op2, $expectedResult) {
-    global $t0;
+function cmp($v1, $v2, $expect) {
+    global $iterations;
 
-    $start = microtime(true);
-    for ($i = 0; $i < 1000000; ++$i) {
-        $rs = ($op1 == $op2);
-    }
-    $end = microtime(true);
+    // equal
+    $t1 = microtime(true);
+    for ($i=0; $i<$iterations; ++$i) $rs = ($v1 == $v2);
+    $t2 = microtime(true);
+    $ok = ($rs === ($expect === 0));
+    result('==', $v1, $v2, ($expect === 0), $rs, $t2-$t1);
+    
+    // lower
+    $t1 = microtime(true);
+    for ($i=0; $i<$iterations; ++$i) $rs = ($v1 < $v2);
+    $t2 = microtime(true);
+    $ok = ($rs === ($expect === -1));
+    result('<', $v1, $v2, ($expect === -1), $rs, $t2-$t1);
 
-    $t   = ($end - $start);
-    $t0 += $t;
+    // lower or equal
+    $t1 = microtime(true);
+    for ($i=0; $i<$iterations; ++$i) $rs = ($v1 <= $v2);
+    $t2 = microtime(true);
+    $ok = ($rs === ($expect === -1 || $expect === 0));
+    result('<=', $v1, $v2, ($expect === -1 || $expect === 0), $rs, $t2-$t1);
 
-    printRs('==', $op1, $op2, $expectedResult, $rs, $t);
+    // greater
+    $t1 = microtime(true);
+    for ($i=0; $i<$iterations; ++$i) $rs = ($v1 > $v2);
+    $t2 = microtime(true);
+    $ok = ($rs === ($expect === 1));
+    result('>', $v1, $v2, ($expect === 1), $rs, $t2-$t1);
+
+    // greater or equal
+    $t1 = microtime(true);
+    for ($i=0; $i<$iterations; ++$i) $rs = ($v1 >= $v2);
+    $t2 = microtime(true);
+    $ok = ($rs === ($expect === 1 || $expect === 0));
+    result('>=', $v1, $v2, ($expect === 1 || $expect === 0), $rs, $t2-$t1);
 }
 
-function cmpGreaterThen($op1, $op2, $expectedResult) {
+function result($op, $v1, $v2, $expectedRs, $rs, $time) {
     global $t0;
 
-    $start = microtime(true);
-    for ($i = 0; $i < 1000000; ++$i) {
-        $rs = ($op1 > $op2);
-    }
-    $end = microtime(true);
+    $t0 += $time;
 
-    $t   = ($end - $start);
-    $t0 += $t;
-
-    printRs('>', $op1, $op2, $expectedResult, $rs, $t);
-}
-
-function cmpLowerThen($op1, $op2, $expectedResult) {
-    global $t0;
-
-    $start = microtime(true);
-    for ($i = 0; $i < 1000000; ++$i) {
-        $rs = ($op1 < $op2);
-    }
-    $end = microtime(true);
-
-    $t   = ($end - $start);
-    $t0 += $t;
-
-    printRs('<', $op1, $op2, $expectedResult, $rs, $t);
-}
-
-function printRs($op, $op1, $op2, $expectedResult, $result, $time) {
-    echo ($result === $expectedResult) ? 'OK   ' : 'FAIL ';
+    echo ($expectedRs === $rs) ? 'OK   ' : 'FAIL ';
     echo str_pad(sprintf(
         "%s %s %s",
-        fmtVar($op1, true),
+        fmt($v1),
         $op,
-        fmtVar($op2, true)
+        fmt($v2)
     ), 44, ' ', STR_PAD_RIGHT);
 
-    printf("\t= %s\ttime = %f\n", fmtVar($result, true), $time);
+    printf("\t= %s\ttime = %f\n", fmt($rs), $time);
 }
 
-function fmtVar($var) {
+function fmt($var) {
     switch (gettype($var)) {
         case 'string':
             return str_replace(array(
@@ -81,105 +80,84 @@ function fmtVar($var) {
 }
 
 echo "String (non-numeric) to string (non-numeric):\n";
-cmpEqual('test', 'test', true);
-cmpEqual('simple', 'test', false);
-
-cmpGreaterThen('test', 'test', false);
-cmpGreaterThen('teste', 'test', true);
-cmpGreaterThen('u', 'test', true);
-cmpGreaterThen('tesu', 'test', true);
-cmpGreaterThen('tess', 'test', false);
-cmpGreaterThen('a', 'Z', true);
-
-cmpLowerThen('test', 'test', false);
-cmpLowerThen('teste', 'test', false);
-cmpLowerThen('s', 'test', true);
-cmpLowerThen('tesu', 'test', false);
-cmpLowerThen('tess', 'test', true);
-cmpLowerThen('Z', 'a', true);
+cmp('str', 'str', 0);
+cmp('str ', 'str', 1);
+cmp('php', 'str', -1);
+cmp('u', 'str', 1);
+cmp('s', 'str', -1);
+cmp('str', 'std', 1);
+cmp('str', 'sts', -1);
+cmp('a', 'Z', 1);
+cmp('Z', 'a', -1);
 
 echo "\nString (non-numeric) to numeric:\n";
-cmpEqual('string', '0', false);
-cmpEqual('string', 0, false);
-cmpEqual('string', '0.1', false);
-cmpEqual('string', 0.1, false);
+cmp('str', '0', 1);
+cmp('str', 0, false);
+cmp('str', '0.1', 1);
+cmp('str', 0.1, false);
 
-cmpGreaterThen('str', '123', true);
-cmpGreaterThen('STR', '123', true);
-cmpGreaterThen(':', '9', true);
-cmpGreaterThen('/', '0', false);
-
-cmpLowerThen('str', '123', false);
-cmpLowerThen('STR', '123', false);
-cmpLowerThen(':', '9', false);
-cmpLowerThen('/', '0', true);
+cmp('str', '123', 1);
+cmp('STR', '123', 1);
+cmp(':', '9', 1);
+cmp('/', '0', -1);
 
 echo "\nString (eventual numeric) to numeric:\n";
-cmpEqual('1string', '1', false);
-cmpEqual('1string', 1, false);
-cmpEqual('string1', '1', false);
-cmpEqual('string1', 1, false);
-cmpEqual(' 1', '1', false);
-cmpEqual(' 1', 1, false);
-cmpEqual("\t1", '1', false);
-cmpEqual("\t1", 1, false);
-cmpEqual("\01", '1', false);
-cmpEqual("\01", 1, false);
+cmp('1str', '1', 1);
+cmp('1str', 1, false);
+cmp('str1', '1', 1);
+cmp('str1', 1, false);
+cmp(' 1', '1', -1);
+cmp(' 1', 1, false);
+cmp("\t1", '1', -1);
+cmp("\t1", 1, false);
+cmp("\01", '1', -1);
+cmp("\01", 1, false);
 
 echo "\nString (numeric) to numeric:\n";
-cmpEqual('1.1', 1.1, true);
-cmpEqual('1.1000000000000001', 1.1, true);
-cmpEqual('1e1', '10', false);
-cmpEqual('1E1', '10', false);
-cmpEqual('1e-1', '0.1', false);
-cmpEqual('1E-1', '0.1', false);
-cmpEqual('+1', '1', false);
-cmpEqual('+0', '-0', false);
-cmpEqual('0.99999999999999994', '1', false);
-cmpEqual('0.99999999999999995', '1', false);
+cmp('1.1', 1.1, 0);
+cmp('1.1000000000000001', 1.1, 0);
+cmp('1e1', '10', 1);
+cmp('1E1', '10', 1);
+cmp('1e-1', '0.1', 1);
+cmp('1E-1', '0.1', 1);
+cmp('+1', '1', -1);
+cmp('+0', '-0', -1);
+cmp('0.99999999999999994', '1', -1);
+cmp('0.99999999999999995', '1', -1);
 
-cmpGreaterThen('123', '123', false);
-cmpGreaterThen('124', '123', true);
-cmpGreaterThen('2', '123', true);
+cmp('123', '123', 0);
+cmp('124', '123', 1);
+cmp('2', '123', 1);
 
 echo "\nInteger to numeric:\n";
-cmpEqual(1, 1.0, true);
-cmpEqual(1, '1', true);
-cmpEqual(1, '1.0', true);
-cmpEqual(1, 2, false);
-cmpEqual(1, 1.1, false);
-cmpEqual(-0, +0.0, true);
+cmp(1, 1.0, 0);
+cmp(1, '1', 0);
+cmp(1, '1.0', 0);
+cmp(1, 2, -1);
+cmp(1, 1.1, -1);
+cmp(-0, +0.0, 0);
 
 echo "\nFloat to numeric:\n";
-cmpEqual(1.1, 1.1, true);
-cmpEqual(1.0, '1', true);
-cmpEqual(1.1, '1.1', true);
-cmpEqual(1.1, '1.1000000000000001', true);
-cmpEqual(1.1, 2, false);
+cmp(1.1, 1.1, 0);
+cmp(1.0, '1', 0);
+cmp(1.1, '1.1', 0);
+cmp(1.1, '1.1000000000000001', 0);
+cmp(1.1, 2, -1);
 
 
 echo "\nNULL to string:\n";
-cmpEqual(NULL, '', true);
-cmpEqual(NULL, ' ', false);
-cmpEqual(NULL, '0', false);
+cmp(NULL, '', 0);
+cmp(NULL, ' ', -1);
+cmp(NULL, '0', -1);
 
 echo "\nNULL to integer:\n";
-cmpEqual(NULL, 0, true);
-cmpEqual(NULL, -1, false);
-cmpEQUAL(NULL, 1, false);
-
-cmpGreaterThen(NULL, 0, false);
-cmpGreaterThen(NULL, -1, true);
-cmpGreaterThen(NULL, 1, false);
+cmp(NULL, 0, 0);
+cmp(NULL, -1, 1);
+cmp(NULL, 1, -1);
 
 echo "\nNULL to float:\n";
-cmpEqual(NULL, 0.0, true);
-cmpEqual(NULL, -1.0, false);
-cmpEQUAL(NULL, 1.0, false);
-
-cmpGreaterThen(NULL, 0.0, false);
-cmpGreaterThen(NULL, -1.0, true);
-cmpGreaterThen(NULL, 1.0, false);
-
+cmp(NULL, 0.0, 0);
+cmp(NULL, -1.0, 1);
+cmp(NULL, 1.0, -1);
 
 printf("\nTotal \t\t\t\t\t\t\ttime = %f\n", $t0);
